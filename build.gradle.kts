37d608f0415c30ea5ef9170249b91adbf77b504a
Grigory <grigoriy.trubnikov@ebs.ru>
2025-08-25 17:18:49 +0300

    feat: подготовка шаблона для микросервисов

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,38 +4,10 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "ru.yandex.practicum"
-version = "0.0.1-SNAPSHOT"
-description = "bank-practicum"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
+//group = "ru.yandex.practicum"
+//version = "0.0.1-SNAPSHOT"
+//description = "bank-practicum"
 
 repositories {
 	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
