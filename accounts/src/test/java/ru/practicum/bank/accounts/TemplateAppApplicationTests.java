73a9496da98d2443478302e6003b54e7786dc275
Grigory <grigoriy.trubnikov@ebs.ru>
2025-09-03 16:01:08 +0300

    feat: рефакторинг структуры

---
+++ accounts/src/test/java/ru/practicum/bank/accounts/TemplateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.bank.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
